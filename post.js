7b48734b7d10add598377b5677746c97539bca3e
K Supraja <suprajak01@gmail.com>
2026-06-08 17:43:07 +0530

    fixed post

---
+++ post.js
@@ -0,0 +1,3 @@
+console.log('Tests finished on platform:', process.platform);
+console.log('Project:', process.env.PROJECT_NAME || 'Playwright-TestMu-Demo');
+console.log('Artifacts collected from playwright-report/ and test-results/');
